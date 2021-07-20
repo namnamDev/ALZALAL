bc8934e36ad80db516f19f3469da787ca75527f6
추헌국 <cngjsrnr@naver.com>
2021-07-20 17:32:38 +0900

    add: MmeberRepositoy 주석 달았음

--- Backend/src/main/java/com/ssafy/common/repository/MemberRepository.java
+++ Backend/src/main/java/com/ssafy/common/repository/MemberRepository.java
@@ -15,20 +15,24 @@ public class MemberRepository {
 
 	@PersistenceContext
 	private EntityManager em;
-
+	
+	//member 정보 insert
 	public Long save(Member member) {
 		em.persist(member);
 		return member.getMember_no();
 	}
 
+	//member PK를 통한 select
 	public Member findOne(Long id) {
 		return em.find(Member.class, id);
 	}
-			
+	
+	//전체 member select
 	public List<Member> findAll() {
 		return em.createQuery("Select m from Member m", Member.class).getResultList();
 	}
 
+	//name을 통한 select
 	public Member findByName(String name) {
 		Member member = null;
 		try {
@@ -40,6 +44,7 @@ public class MemberRepository {
 		return member;
 	}
 
+	//email을 통한 select
 	public Member findByEmail(String email) {
 		System.out.println("email"+email);
 		Member member = null;
@@ -52,5 +57,4 @@ public class MemberRepository {
 		}
 		return member;
 	}
-
 }
