f7f9cd542c493bc9360e6586e8c79003d1881fac
추헌국 <cngjsrnr@naver.com>
2021-07-27 21:23:52 +0900

    fix: 이미지 가져오기 회원 pk로 가능하게 수정

--- Backend/src/main/java/com/ssafy/common/service/FileServiceImpl.java
+++ Backend/src/main/java/com/ssafy/common/service/FileServiceImpl.java
@@ -10,7 +10,6 @@ import java.nio.file.StandardCopyOption;
 
 import javax.annotation.PostConstruct;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
@@ -19,6 +18,9 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.ssafy.common.FileUploadProperties;
+import com.ssafy.common.domain.member.Member;
+import com.ssafy.common.jwt.util.SecurityUtil;
+import com.ssafy.common.repository.MemberRepository;
 
 @Service
 @Transactional
@@ -38,11 +40,26 @@ public class FileServiceImpl implements FileService {
 			e.printStackTrace();
 		}
 	}
+	
+	@Autowired
+	private MemberRepository memberRepository;
 
+	//유저 pk를 이름으로 이미지 저장
 	@Override
 	public String saveFile(MultipartFile multipartFile) {
 
-		String fileName = multipartFile.getOriginalFilename();
+		Member member = memberRepository.findByNo(SecurityUtil.getCurrentMemberId())
+				.orElseThrow(() -> new IllegalStateException("로그인 유저정보가 없습니다"));
+
+		String originalfileName = multipartFile.getOriginalFilename();
+		String[] tmpstr=originalfileName.split("\\.");
+		
+		
+		if(tmpstr.length<1)
+			throw new IllegalStateException("유효하지 않은 확장자입니다");
+		
+		String fileName = Long.toString(member.getNo())+"."+tmpstr[tmpstr.length-1];
+		
 		Path location = this.dirLocation.resolve(fileName);
 		try {
 			/* 실제 파일이 upload 되는 부분 */
