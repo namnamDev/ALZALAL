323321eac1e0e6a53cffaa085dd2c971c244067b
남근형 <magicom98@gmail.com>
2021-07-28 13:27:31 +0900

    fix)브런치 명 변경 Mytest>InsertQuerydslTEST

--- Backend/src/main/java/com/ssafy/common/service/ArticleServiceImpl.java
+++ Backend/src/main/java/com/ssafy/common/service/ArticleServiceImpl.java
@@ -66,60 +66,60 @@ public class ArticleServiceImpl implements ArticleService{
   @Override
   public Map<String, Object>deleteArticle(String articleClass,long articlePk){
 	  Map<String,Object>res = new HashMap<String,Object>();
-//	  String msg = "";
-//	  Member member = memberRepository.findByNo(SecurityUtil.getCurrentMemberId())
-//				.orElseThrow(() -> new IllegalStateException("로그인 유저정보가 없습니다"));
-//	  	if(articleClass.equals("article")){
-//	  		
-//	  		//삭제 전 단건조회
-//	    	Article article = ars.sltOne(articlePk);
-//	    	if (article == null) {
-//	    		msg = "존재하지 않는 게시글입니다.";
-//	    	}else {
-//	    		//삭제 전 유저 일치 조회
-//	    		if(member != article.getMember()){
-//	    		msg ="자신의 글만 삭제할 수 있습니다.";
-//	    		}else{
-//		    		long deletedArticle = ars.articleDelete(articlePk);
-//		    		res.put("deletedArtcle", deletedArticle);
-//		    		msg = "성공적으로 삭제되었습니다.";
-//	    		}
-//	    	}
-//
-//	    	res.put("msg",msg);
-//
-//	    }
-//	    else if(articleClass.equals("discussion")){
-//	    	
-//	    }
+	  String msg = "";
+	  Member member = memberRepository.findByNo(SecurityUtil.getCurrentMemberId())
+				.orElseThrow(() -> new IllegalStateException("로그인 유저정보가 없습니다"));
+	  	if(articleClass.equals("article")){
+	  		
+	  		//삭제 전 단건조회
+	    	Article article = ars.sltOne(articlePk);
+	    	if (article == null) {
+	    		msg = "존재하지 않는 게시글입니다.";
+	    	}else {
+	    		//삭제 전 유저 일치 조회
+	    		if(member != article.getMember()){
+	    		msg ="자신의 글만 삭제할 수 있습니다.";
+	    		}else{
+		    		long deletedArticle = ars.articleDelete(articlePk);
+		    		res.put("deletedArtcle", deletedArticle);
+		    		msg = "성공적으로 삭제되었습니다.";
+	    		}
+	    	}
+
+	    	res.put("msg",msg);
+
+	    }
+	    else if(articleClass.equals("discussion")){
+	    	
+	    }
 	  return res;
   }
 @Override
 public Map<String, Object> updateArticle(String articleClass, long articlePk, Map<String, Object> req) {
 	Map<String,Object>res = new HashMap<String,Object>();
-//	String msg = "";
-//	Member member = memberRepository.findByNo(SecurityUtil.getCurrentMemberId())
-//			.orElseThrow(() -> new IllegalStateException("로그인 유저정보가 없습니다"));
-//	if(articleClass.equals("article")){
-//		//수정 전 단건 조회
-//		Article article = ars.sltOne(articlePk);
-//		if (article == null) {
-//    		msg = "존재하지 않는 게시글입니다.";
-//    	}else {
-//    		if(member != article.getMember()){
-//	    		msg ="자신의 글만 삭제할 수 있습니다.";
-//	    		}else{
-//	        		String content = (String) req.get("content");
-//	        		long updatedArticle = ars.updateArticle(articlePk,content);
-//	        		res.put("updatedArtcle", updatedArticle);
-//	        		msg = "성공적으로 수정되었습니다.";
-//	    		}
-//    	}
-//
-//    	res.put("msg",msg);
-//	}else if(articleClass.equals("discussion")){
-//    	
-//    }
+	String msg = "";
+	Member member = memberRepository.findByNo(SecurityUtil.getCurrentMemberId())
+			.orElseThrow(() -> new IllegalStateException("로그인 유저정보가 없습니다"));
+	if(articleClass.equals("article")){
+		//수정 전 단건 조회
+		Article article = ars.sltOne(articlePk);
+		if (article == null) {
+    		msg = "존재하지 않는 게시글입니다.";
+    	}else {
+    		if(member != article.getMember()){
+	    		msg ="자신의 글만 삭제할 수 있습니다.";
+	    		}else{
+	        		String content = (String) req.get("content");
+	        		long updatedArticle = ars.updateArticle(articlePk,content);
+	        		res.put("updatedArtcle", updatedArticle);
+	        		msg = "성공적으로 수정되었습니다.";
+	    		}
+    	}
+
+    	res.put("msg",msg);
+	}else if(articleClass.equals("discussion")){
+    	
+    }
 	return res;
 }
   
