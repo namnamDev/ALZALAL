a841d5740f25d9376c12b4da7ecdb3a76be1187f
남근형 <magicom98@gmail.com>
2021-08-04 00:58:28 +0900

    게시글 조회시 게시글-알고리즘 같이 조회 게시글 수정시 게시글-알고리즘도 수정됨

--- Backend/src/main/java/com/ssafy/common/repository/article/Article_AlgorithmRepositoryCustom.java
+++ Backend/src/main/java/com/ssafy/common/repository/article/Article_AlgorithmRepositoryCustom.java
@@ -3,14 +3,23 @@ package com.ssafy.common.repository.article;
 import java.util.List;
 import java.util.Optional;
 
+import com.querydsl.jpa.impl.JPAQuery;
 import com.ssafy.common.domain.Algorithm;
 import com.ssafy.common.domain.article.Article;
 import com.ssafy.common.domain.article.Article_Algorithm;
+import com.ssafy.common.dto.ArticleDTO;
+import com.ssafy.common.dto.Article_AlgorithmDTO;
 
 public interface Article_AlgorithmRepositoryCustom {
 
 	List<Article_Algorithm> findAll();
 
 	Optional<Article_Algorithm> sltOne(Algorithm algo, Article article);
+
+	List<Article_Algorithm> sltMultiByArticle(Article article);
+
+	Long deleteArtiAlgo(Algorithm algo);
+
+	List<Article_AlgorithmDTO> sltMultiByArticleDTO(ArticleDTO article);
 
 }
