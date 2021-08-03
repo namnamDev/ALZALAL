98c8a5e138105189b3a1979312aa7f154106ed02
추헌국 <cngjsrnr@naver.com>
2021-08-04 00:09:50 +0900

    fix: 생성자 빠진거 추가

--- Backend/src/main/java/com/ssafy/common/domain/problem/Problem_Site_List.java
+++ Backend/src/main/java/com/ssafy/common/domain/problem/Problem_Site_List.java
@@ -4,11 +4,15 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Problem_Site_List {
 	
 	@Id
