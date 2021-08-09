3e10409be4e5e9c12386d0e3e28fd6d57e61fc49
추헌국 <cngjsrnr@naver.com>
2021-08-09 19:01:19 +0900

    fix: Time_zone UTC -> Asia/Seoul로 변경

--- Backend/src/main/java/com/ssafy/common/CommonProjectApplication.java
+++ Backend/src/main/java/com/ssafy/common/CommonProjectApplication.java
@@ -1,5 +1,9 @@
 package com.ssafy.common;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -12,6 +16,10 @@ public class CommonProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommonProjectApplication.class, args);
+	}
+	@PostConstruct
+	public void initApplication() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
