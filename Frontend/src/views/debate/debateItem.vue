a33eb9fc66f18c7aeb4af157381a64fe25676d17
박종익 <pji3504@naver.com>
2021-08-10 15:06:09 +0900

    토론목록 불러오기 구현

---
+++ Frontend/src/views/debate/debateItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="row">
+    <div class="col-lg-3 col-md-2 col-sm-3 col-0"></div>
+    <div class="debate col-lg-6 col-md-10 col-sm-9 col-12">
+        <div class="contentsWrap">
+            <p class="title">{{this.discussionCompName}}     <span>{{this.discussionCompProblem}}</span></p>
+            <p>{{this.discussionHostName}}</p>                
+        </div>
+        <div class="btn-group wrap col-12 col-lg-12 col-ml-12">
+            <div class="col box1 like-comment">
+              <i class="far fa-comment-dots mx-2"></i>
+              <span >{{this.discussCommentCount}}</span>
+              <div><p class="date">{{this.discussionDate}}</p></div>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        discussionHostName: String,
+        discussionHostNo: Number,
+        discussionCompName: String,
+        discussionDate: String,
+        discussionNo: Number,
+        discussionCompProblem: String,
+        discussCommentCount: Number
+    }
+}
+</script>
+
+<style>
+
+</style>
--- Frontend/src/views/debate/debateList.vue
+++ Frontend/src/views/debate/debateList.vue
@@ -1,65 +1,94 @@
 <template>
   <div class="container feed">
-    <div class="row">
-      <div class="col-lg-3 col-md-2 col-sm-3 col-0"></div>
-      <div class="debate col-lg-6 col-md-10 col-sm-9 col-12 ">
-        <div class="debate-request" align="right">
-          <button class="btn btn-request" @click="clickCreate">토론방 생성하기</button>
+      <div class="row">
+        <div class="col-lg-3 col-md-2 col-sm-3 col-0"></div>
+        <div class="debate feed-card col-lg-6 col-md-10 col-sm-9 col-12">
+            <div class="contentsWrap">
+                <p class="title">카카오 2016 인턴  <span>1번문제</span></p>
+                <p>카카오</p>  
+            </div>
+            <div class="btn-group wrap col-12 col-lg-12 col-ml-12">
+                <div class="col box1 like-comment">
+                  <i class="far fa-comment-dots mx-2"></i>
+                  <span >2</span>
+                  <div><p class="date">2016-01-03 09:00:00</p></div>
+                </div>
+            </div>
         </div>
-        <div class="col-md-12">
-          <div class="table-wrap">
-          <table class="table table-striped">
-          <thead>
-            <tr>
-              <th>토론방 번호</th>
-              <th>개최 기관</th>
-              <th>대회 이름</th>
-              <th>문제번호</th>
-              <th>일시</th>
-              <th>상태</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1001</th>
-              <td>프로그래머스</td>
-              <td>2020상반기대회</td>
-              <td>1번 문제</td>
-              <td>2020/03/01</td>
-              <td><a href="#" class="btn btn-success">Progress</a></td>
-            </tr>
-            <tr>
-              <th scope="row">1001</th>
-              <td>코드포스</td>
-              <td>무슨무슨대회</td>
-              <td>몇번문제</td>
-              <td>2021/04/25</td>
-              <td><a href="#" class="btn btn-warning">Open</a></td>
-            </tr>
-            <tr>
-              <th scope="row">1001</th>
-              <td>엣코더</td>
-              <td>무슨무슨대회</td>
-              <td>몇번문제</td>
-              <td>2023/01/01</td>
-              <td><a href="#" class="btn btn-danger">On hold</a></td>
-            </tr>
-          </tbody>
-          </table>
-          </div>
-          </div>
       </div>
-    </div>
+      <debateItem v-for="item,index in discussion" :key="index"
+        :discussionHostName="item.discussCompHostNo.name"
+        :discussionHostNo="item.discussCompHostNo.no"
+        :discussionCompName="item.discussCompName"
+        :discussionDate="item.discussDate"
+        :discussionNo="item.discussNo"
+        :discussionCompProblem="item.disscussCompProblem"
+        :discussCommentCount="item.discussCommentCount"
+      ></debateItem>
+      <infinite-loading @infinite="infiniteHandler" spinner="sprial">
+        <div slot="no-more" style="color: rgb(102, 102, 102); font-size: 14px; padding: 10px 0px;">목록의 끝입니다 :)</div>
+      </infinite-loading>
   </div>
 </template>
 
 <script>
+import debateItem from '@/views/debate/debateItem.vue'
+import axios from 'axios';
+const SERVER_URL = process.env.VUE_APP_SERVER_URL
+import InfiniteLoading from 'vue-infinite-loading';
 export default {
+  components:{
+    InfiniteLoading,
+    debateItem
+  },
+  data(){
+    return{
+      discussion:[],
+      page:0
+    }
+  },
+  created: function(){
+   
+  },
   methods: {
-    clickCreate: function() {
-      this.$router.push({'name':'createDebate'})
-    },
-  }
+    infiniteHandler($state) {
+     axios({
+        method: 'get',
+        url: `${SERVER_URL}/articlelist/discussion`+"?page=" + (this.page),
+        headers: this.getToken,
+      })   //토론 불러와
+      .then(res =>{     
+        console.log(res)
+        console.log(res.data.discussion.length)             
+        setTimeout(() => {
+            if(res.data.discussion.length) {
+              this.discussion = this.discussion.concat(res.data.discussion)
+              $state.loaded()
+              this.page += 1
+              // 끝인지 판별
+              if(res.data.discussion.length / 20 < 1) {
+                $state.complete()
+              }
+            } else {
+              // 끝 지정(No more data)
+              $state.complete()
+            }
+          }, 1000)
+      })
+      .catch((err)=>{  // 실패하면 error
+        console.log(err)
+      })
+    }
+  },
+  computed: {
+    getToken(){
+      const token = localStorage.getItem('jwt')
+      const config = {
+        Authorization: `Bearer ${token}`
+      }
+      return config
+    }
+	},
 }
 </script>
 
@@ -71,27 +100,57 @@ export default {
   transform: translateX(-50%);
   margin-left:0;
 }
+.row{
+  margin-top: 100px;
+}
 .debate {
   height: 300px;
   font-size: 1vw;
   line-height: 2;
-}
-.btn{
-  width:100%
-}
-.btn-request {
-  float: left;
-  padding: 0.5rem 1.5rem;
-  font-weight: 700;
-  border-radius: .1rem;
-}
-.table-wrap{
-  /* margin-top: 10%; */
 }
 @media (max-width:577px) {
   .feed{
     left: 0;
     transform: translateX(0);
   }
+}
+
+
+
+
+.feed-card {
+  box-sizing: content-box;
+  /* box-shadow: 0 0 0 1px #ddd; */
+  color: #000;
+  float: left;
+  border-radius: 5px;
+  overflow: hidden;
+  
+}
+.contentsWrap {
+  box-sizing: border-box;
+  padding: 12px;
+  float: left;
+}
+.title {
+  color:#000;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  word-wrap:break-word; 
+  line-height: 1.5em;
+  font-size: 25px;
+  font-weight: 550;
+  margin: 0 0 8px;
+  white-space: normal;
+}
+.date {
+  float: right;
+  font-size: 15px;
+  color:rgba(0, 0, 0, .5);
+  
+}
+.comment{
+  margin-right: 30px;
 }
 </style>
