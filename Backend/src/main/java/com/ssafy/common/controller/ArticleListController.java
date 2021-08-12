5518bab99a08d738251a3c7d9cf929cc44d11b63
설재홍 <seol940601@gmail.com>
2021-08-12 06:54:57 +0000

    Update Backend/src/main/java/com/ssafy/common/controller/AlgorithmController.java, Backend/src/main/java/com/ssafy/common/controller/ArticleListController.java, Backend/src/main/java/com/ssafy/common/jwt/config/SecurityConfig.java files

--- Backend/src/main/java/com/ssafy/common/controller/ArticleListController.java
+++ Backend/src/main/java/com/ssafy/common/controller/ArticleListController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/articlelist")
+@RequestMapping("/articlelist")
 public class ArticleListController {
   @Autowired
   ArticleService as;
